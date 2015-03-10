39941d611d05724cd9c21e051cee0a212b30e991
Christian Rubiales <christianrubiales@yahoo.com>
2015-03-11 01:18:43 +0800

    APlusBProblem

---
+++ easy/APlusBProblem.java
@@ -0,0 +1,11 @@
+class APlusBProblem {
+    /*
+     * param a: The first integer
+     * param b: The second integer
+     * return: The sum of a and b
+     */
+    public int aplusb(int a, int b) {
+        // Click submit, you will get Accepted!
+        return a - -b;
+    }
+};
